6f7581276cb961ab09869fe6fc120a7135c6ae9b
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2025-01-27 15:35:16 +0100

    resolving sync issues

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# First assesment - intern DEV@WORK
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Swal from "sweetalert2";
+import "./App.css"; 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const sumNumbers = async () => {
+    const { value: number1 } = await Swal.fire({
+      title: "Enter a number",
+      input: "number",
+      inputPlaceholder: "Enter a number",
+      color: "#000",
+      confirmButtonColor: "#000",
+    });
+
+    if (!number1) return;
+
+    const { value: number2 } = await Swal.fire({
+      title: "Enter another number",
+      input: "number",
+      inputPlaceholder: "Enter another number",
+      color: "#000",
+      confirmButtonText: "Submit",
+      confirmButtonColor: "#000",
+    });
+
+    if (!number2) return;
+
+    const sum = parseFloat(number1) + parseFloat(number2);
+
+    Swal.fire({
+      title: "Result",
+      text: `The sum of the two numbers is ${sum}`,
+      icon: "success",
+      color: "#000",
+      confirmButtonText: "Close",
+      confirmButtonColor: "#000",
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="card">
+      <h1>Sum Calculator</h1>
+      <button onClick={sumNumbers}>Start Calculation</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
